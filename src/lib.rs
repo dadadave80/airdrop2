df75050682a2c5bc9e4dba143e5f352dc71a153a
David Dada <daveproxy80@gmail.com>
2025-10-01 21:21:47 +0100

    chore: add keygen logic

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn keygen() {}
+    fn keygen() {
+        // Create a new keypair
+        let kp = Keypair::new();
+        println!("You've generated a new Solana wallet: {}\n", kp.pubkey());
+        println!("To save your wallet, copy and paste the following into a JSON file:");
+        println!("{:?}", kp.to_bytes());
+    }
     #[test]
     fn claim_airdrop() {}
     #[test]
