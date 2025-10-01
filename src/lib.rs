aef8a2682157d7d01b42c721466c7013d4d5bbaf
David Dada <daveproxy80@gmail.com>
2025-10-01 21:32:59 +0100

    chore: add base58 to wallet conversion

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,17 @@ mod tests {
         println!("To save your wallet, copy and paste the following into a JSON file:");
         println!("{:?}", kp.to_bytes());
     }
+
+    #[test]
+    fn base58_to_wallet() {
+        println!("Input your private key as a base58 string:");
+        let stdin = io::stdin();
+        let base58 = stdin.lock().lines().next().unwrap().unwrap();
+        println!("Your wallet file format is:");
+        let wallet = bs58::decode(base58).into_vec().unwrap();
+        println!("{:?}", wallet);
+    }
+
     #[test]
     fn claim_airdrop() {}
     #[test]
