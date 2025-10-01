62e68afbb460eca0b9ea39c8b74c8aa3a00db84a
David Dada <daveproxy80@gmail.com>
2025-10-01 21:33:41 +0100

    chore: add wallet to base58 logic

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,28 @@ mod tests {
     }
 
     #[test]
+    fn wallet_to_base58() {
+        println!("Input your private key as a JSON byte array (e.g. [12,34,...]):");
+        let stdin = io::stdin();
+        let wallet = stdin
+            .lock()
+            .lines()
+            .next()
+            .unwrap()
+            .unwrap()
+            .trim_start_matches('[')
+            .trim_end_matches(']')
+            .split(',')
+            .map(|s| s.trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        println!("Your Base58-encoded private key is:");
+        let base58 = bs58::encode(wallet).into_string();
+        println!("{:?}", base58);
+    }
+
+    #[test]
     fn claim_airdrop() {}
+
     #[test]
     fn transfer_sol() {}
 }
