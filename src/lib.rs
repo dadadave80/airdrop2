b52ad30ae8fd8ddf9e5d657cafedbacb70bb5620
David Dada <daveproxy80@gmail.com>
2025-10-01 20:37:19 +0100

    chore: add test functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
+    use solana_sdk;
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn keygen() {}
+    #[test]
+    fn claim_airdrop() {}
+    #[test]
+    fn transfer_sol() {}
 }
