e576e7bb5e534dfa68fb6b6190a690c63df663a9
David Dada <daveproxy80@gmail.com>
2025-10-02 08:02:35 +0100

    chore: add verify signature hash that fails for some reason

--- src/lib.rs
+++ src/lib.rs
@@ -75,5 +75,19 @@ mod tests {
     }
 
     #[test]
-    fn transfer_sol() {}
+    fn transfer_sol() {
+        // Load your devnet keypair from file
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+
+        // Generate a signature from the keypair
+        let pubkey = keypair.pubkey();
+        let message_bytes = b"I verify my Solana Keypair!";
+        let sig = keypair.sign_message(message_bytes);
+        let sig_hashed = hash(sig.as_ref());
+
+        // Verify the signature using the public key
+        match sig.verify(&pubkey.to_bytes(), &sig_hashed.to_bytes()) {
+            true => println!("Signature verified"),
+            false => println!("Verification failed"),
+        }
 }
